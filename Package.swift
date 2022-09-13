66f87f3f274c0b28275f59a7d916b0f0e2a0adf3
Shahbaz Saleem <shahbaz@saleems.me>
2022-09-13 16:03:10 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "TPKeyboardAvoiding",
+    platforms: [
+      .iOS(.v12)
+    ],
     products: [
         .library(name: "TPKeyboardAvoiding", targets: ["TPKeyboardAvoiding"])
     ],
